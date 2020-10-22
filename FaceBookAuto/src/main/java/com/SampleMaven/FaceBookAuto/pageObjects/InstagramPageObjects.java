33ca4ba34319eeb7bfd76993b146855c9043038c
Pragna <pragna.varun123@gmail.com>
2020-10-22 15:33:41 +1100

    Added Instagram, People and Recovery Page Objects

---
+++ FaceBookAuto/src/main/java/com/SampleMaven/FaceBookAuto/pageObjects/InstagramPageObjects.java
@@ -0,0 +1,21 @@
+package com.SampleMaven.FaceBookAuto.pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class InstagramPageObjects {
+	
+public static WebDriver driver;
+	
+	///////////Login Objects 
+	
+	////Login email 
+	@FindBy(name="email")
+	public WebElement LoginEmailOnFBHome;
+	
+	//Login Password
+	@FindBy(name="pass")
+	public WebElement LoginPasswordOnFBHome;
+
+}
